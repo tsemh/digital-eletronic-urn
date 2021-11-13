f1d81c38a8f9d59ecc1b192cb4afcee44a9c20b7
tiago silva <tiago_barbosa-silva@hotmail.com>
2021-11-13 18:03:47 -0300

    exercicio

---
+++ assets/js/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Mario',
+                partido: 'ABC',
+                fotos: [
+                    {url:'mario.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Moranguinho',
+                partido: 'DEFG',
+                fotos: [
+                    { url:'marango.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero:'99',
+                nome: 'Popaye',
+                partido: 'ABC',
+                vice: 'Galinha pintadinha',
+                fotos: [
+                    { url:'pop.jpg', legenda: 'Prefeito' },
+                    { url:'galinha.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Araragi Koyomi',
+                partido: 'QWERTY',
+                vice: 'Dio',
+                fotos: [
+                    { url:'araragi.jpg', legenda: 'Prefeito' },
+                    { url:'dio.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            }
+
+        ]
+    }
+]
